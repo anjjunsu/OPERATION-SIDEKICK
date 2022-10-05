621007a86a5eed5733ebda181340b672a4d48178
Junsu An <90178502+anjjunsu@users.noreply.github.com>
2022-10-05 12:23:27 -0700

    1.1.0 Sign in

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert("Hi");
+const MAX_USERNAME_LENGTH = 15;
+
+const signInForm = document.getElementsById("sign-in-form");
+const signInInput = signInForm.querySelector("input");
+const signInButton = signInForm.querySelector("button");
+
+function onSignInClick() {
+    const username = signInInput.value;
+    
+};
+
+signInButton.addEventListner("click", onSignInClick);
