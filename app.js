ced4073a2ba8fd9d1adea7216823ec7cdb634033
Junsu An <anjjunsu@gmail.com>
2022-10-05 18:32:13 -0700

    commit to pull

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("Hi");
+const title = document.querySelector("div.ddiv:first-child h1");
+
+function handleTitleClick() {
+    console.log("title was clicked");
+}
+
+title.addEventListener("click", handleTitleClick);
