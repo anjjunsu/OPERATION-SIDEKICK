498c290b6e9bf26563bb746f5305f70712f7de8a
Junsu An <anjjunsu@gmail.com>
2022-10-06 00:01:51 -0700

    Implemented Clock

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,17 @@
+const ONE_SEC = 1000; 
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
     // Let's not use Javascript Date object
+    // Use Intl instead
     const date = new Date();
-    console.log(date);
-    clock.innerText = date;
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+
+    clock.innerText = `${hours}:${minutes}:${seconds}` ;
 }
 
 getClock();
+setInterval(getClock, ONE_SEC);
 
