d4efc4cea1d28c578a4f2de3d92b2174c103d913
Junsu An <anjjunsu@gmail.com>
2022-10-06 19:55:58 -0700

    Random Img

---
+++ js/backgroud.js
@@ -0,0 +1,20 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg"
+];
+
+const randomImg = images[Math.floor(Math.random() * images.length)];
+const bgImg = document.createElement("img");
+
+bgImg.src = `img/${randomImg}`;
+
+document.body.appendChild(bgImg);
+
